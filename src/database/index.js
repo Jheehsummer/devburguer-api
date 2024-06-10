e165f08a39578d3626a47893c1a2013c55983cca
Jhennyfer Costa <jheeh.summer1927@gmail.com>
2024-06-10 18:52:42 -0300

    first commit

---
+++ src/database/index.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+import configDatabase from '../config/database';
+import User from "../app/models/User";
+import Product from '../app/models/Products';
+import Category from '../app/models/Category';
+
+
+const models = [User, Product, Category];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(configDatabase);
+    models.map((model) => model.init(this.connection));
+  }
+}
+
+export default new Database();
+    
